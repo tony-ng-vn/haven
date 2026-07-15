02b2992fb91ecaba094f0d81b036203a354e18ba
Tony Nguyen <17thien.nguyen@gmail.com>
2026-07-15 12:03:13 -0700

    fix: drop tsx extension from app import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ConvexReactClient } from "convex/react";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
